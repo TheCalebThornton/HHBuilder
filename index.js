9625f26e7d180e1fcf18456e4c265e6380509690
Caleb Thornton <cthornton777@yahoo.com>
2017-10-11 19:16:11 -0400

    CT - function stubs

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 //Bind submit  button
+//TODO onload wrapper -> bind Submit and Add
 
+function submitHousehold(){
+  //TODO create an ajax request to a fake server. Send data as JSON in POST request use debug DOM element
+}
+
+function addPerson(){
+
+}
+
+function removePerson(){
+
+}
 
 function validatePerson(){
 
 }
+
+function appendPersonToHouseHold(){
+  //TODO check if a div has been created for the list of members in a HH
+
+  //TODO if not, invoke initializeHouseHoldContainer
+  //TODO append html to the ordered list with #householdList id
+  //TODO add X button for deleting household members; invoke bindRemovePerson
+}
+
+function initializeHouseholdContainer(){
+  //TODO add html <div id="householdContainer"><ol id="householdList"></ol></div>
